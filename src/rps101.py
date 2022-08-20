2b194ebc5abaedbaae23cf148d1644b2421ab769
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-21 00:24:27 +0800

    Create rps101.py

---
+++ src/rps101.py
@@ -0,0 +1,30 @@
+from requests import get
+
+
+class RPS101:
+	def __init__(self):
+		self.api = "https://rps101.pythonanywhere.com/api/v1"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+		}
+
+
+	def get_all_objects(self):
+		return get(
+			f"{self.api}/objects/all",
+			headers=self.headers).json()
+
+
+	def get_object_info(self, object_name: str):
+		return get(
+			f"{self.api}/objects/{object_name}",
+			headers=self.headers).json()
+
+
+	def get_match_result(
+			self,
+			object_one: str,
+			object_two: str):
+		return get(
+			f"{self.api}/match?object_one={object_one}&object_two={object_two}",
+			headers=self.headers).json()
